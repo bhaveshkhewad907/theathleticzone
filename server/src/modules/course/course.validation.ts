c01f2266ad1232ea125ca9574d12d319dcc18b02
bhaveshkhewad907 <bhaveshkhewad907@gmail.com>
2026-06-14 23:58:39 +0530

    last chance

--- server/src/modules/course/course.validation.ts
+++ server/src/modules/course/course.validation.ts
@@ -1,11 +1,27 @@
 import { z } from "zod";
 
 export const createCourseSchema = z.object({
-  title: z.string().min(3),
-  description: z.string().min(10),
-  thumbnailUrl: z.string().url(),
-  videoUrl: z.string().min(3).optional(),
-  price: z.coerce.number().min(0),
+  meta: z.object({
+    title: z.string().min(1, "Title is required"),
+    description: z.string().min(1, "Description is required"),
+    tier: z.string().min(1, "Tier is required"),
+    targetDeficit: z.string().min(1, "Target Deficit is required"),
+    coverImageUrl: z.string().min(1, "Cover Image is required"),
+  }),
+  price: z.number().min(0, "Price is required"),
+  isActive: z.boolean().optional(),
 });
 
-export const updateCourseSchema = createCourseSchema.partial();
+export const updateCourseSchema = z.object({
+  meta: z
+    .object({
+      title: z.string().optional(),
+      description: z.string().optional(),
+      tier: z.string().optional(),
+      targetDeficit: z.string().optional(),
+      coverImageUrl: z.string().optional(),
+    })
+    .optional(),
+  price: z.number().optional(),
+  isActive: z.boolean().optional(),
+});
