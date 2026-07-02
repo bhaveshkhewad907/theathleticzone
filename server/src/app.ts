52f0121ea9d209ed7f4c7179da81652a56719558
bhaveshkhewad907 <bhaveshkhewad907@gmail.com>
2026-07-02 10:14:23 +0530

    size increased for courses

--- server/src/app.ts
+++ server/src/app.ts
@@ -117,7 +117,8 @@ app.use(globalLimiter);
 // 7. BODY PARSERS & COOKIES
 // ==========================================
 app.use(cookieParser());
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // ==========================================
 // 8. DATA SANITIZATION
